a33114965a8cf79fede4fcc143dcb0d29d16b89a
Pitchaya5904 <Pitchaya5904@gmail.com>
2025-04-26 15:48:15 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === "production" ? "/Detalk-App/" : "/",
-    server: {
-      host: "::",
-      port: 8080,
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "dist/index.html", // หลัง build เสร็จ
+          dest: "", // ไปวางที่ root เป็น 404.html
+          rename: "404.html",
+        },
+      ],
+    }),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [react(), mode === "development" && componentTagger()].filter(
-      Boolean
-    ),
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-  };
-});
+  },
+  base: "/Detalk-App/",
+}));
